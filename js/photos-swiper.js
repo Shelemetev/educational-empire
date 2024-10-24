a05bc2da5cd970e2063cc7f030c33f0aa4f825ab
Egor <egor.shelemetev228@mail.ru>
2024-10-24 22:42:20 +1000

    fix

--- js/photos-swiper.js
+++ js/photos-swiper.js
@@ -1,13 +1,18 @@
 
 const PhotosSwiper = new Swiper('.photos__swiper', {
-    spaceBetween: 8,
-    slidesPerView: "auto",
-    loop: true,
-    freeMode: true,
-    preventInteractionOnTransition: true,
-    autoplay: {
-      delay: 1,
-    },
-    speed: 5000,
-    centeredSlides:true,
+  loop: true,
+  freeMode: true,
+  spaceBetween: 8,
+  grabCursor: true,
+  slidesPerView: "auto",
+  loop: true,
+  autoplay: {
+    delay: 1,
+    disableOnInteraction: true
+  },
+  freeMode: true,
+  speed: 5000,
+  freeModeMomentum: false,
+  preventInteractionOnTransition: true,
+    
 })
